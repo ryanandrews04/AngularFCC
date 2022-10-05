dd2a1d2ce49fb3847b197af6da76524f5d057faa
Ryan <rcandrews44@gmail.com>
2022-10-05 11:23:35 -0400

    5:54:18

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DoCheck, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+  styleUrls: ['./rooms.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RoomsComponent implements OnInit {
+export class RoomsComponent implements OnInit, DoCheck, AfterViewInit {
 
   hotelName = 'Hilton Hotel';
 
@@ -14,14 +16,28 @@ export class RoomsComponent implements OnInit {
 
   hideRooms = false;
 
+  selectedRoom!: RoomList;
+
   rooms: Room = {
     totalRooms: 20,
     availableRooms: 10,
     bookedRooms: 5
   }
 
-  roomList: RoomList[] = [
-    {
+  title: string = 'Room List';
+
+  roomList: RoomList[] = []
+
+  @ViewChild(HeaderComponent, {static: true}) headerComponent!: HeaderComponent;
+
+  constructor() { }
+  
+  
+  ngOnInit(): void {
+
+    console.log(this.headerComponent);
+    
+    this.roomList = [{
       roomNumber: 1,
       roomType: 'Deluxe Room',
       amenities: 'Air Conditioner, Free Wifi, TV, Bathroom, Kitchen',
@@ -39,7 +55,7 @@ export class RoomsComponent implements OnInit {
       photos: "",
       checkinTime: new Date('11-Nov-2021'),
       checkoutTime: new Date('12-Nov-2021'),
-      rating: 3.4
+      rating: 3.483946
     },
     {
       roomNumber: 3,
@@ -50,16 +66,38 @@ export class RoomsComponent implements OnInit {
       checkinTime: new Date('11-Nov-2021'),
       checkoutTime: new Date('12-Nov-2021'),
       rating: 2.6
-    }
-  ]
+    }]
+  }
 
-  constructor() { }
+  ngDoCheck(): void {
+    console.log('on changes is called');
+  }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    console.log(this.headerComponent);
   }
 
   toggle() {
     this.hideRooms = !this.hideRooms;
+    this.title = "Rooms List"
   }
 
+  selectRoom(room: RoomList){
+    this.selectedRoom = room;
+  }
+
+  addRoom(){
+    const room: RoomList = {
+      roomNumber: 4,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wifi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos: "",
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+      rating: 4.5
+    }
+
+    this.roomList = [...this.roomList, room];
+  }
 }
